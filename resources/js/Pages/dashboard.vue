568f71126ee588e137498c948a1cdc8339eaae06
mohamedDev69 <mohamedchettah0208@gmail.com>
2024-11-11 11:54:24 +0100

    first commit

---
+++ resources/js/Pages/dashboard.vue
@@ -0,0 +1,17 @@
+<script setup>
+import {router} from "@inertiajs/vue3";
+import {route} from "ziggy-js";
+
+const logout = () => {
+    router.post(route('logout'));
+};
+</script>
+
+<template>
+    <h1 class="text-2xl font-bold">Dashboard</h1>
+    <a :href="logout" class="text-white bg-red-500 px-4 py-2 rounded-lg">
+        Logout
+    </a>
+</template>
+
+
